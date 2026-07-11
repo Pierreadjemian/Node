e3c985778fc5bdde6a1ea96bf06635443a08c00b
Pierreadjemian <Pierreadjemian@gmail.com>
2026-07-11 22:20:04 +0300

    Add package-lock.json

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ const { readFile , readFileSync } = require('fs');
 const txt = readFileSync ('./hello.txt' , 'utf8' , (err, txt) =>{
 console.log(txt)
 });
-console.log ('do this ASAP')
+console.log ('Im alive and deployed')
