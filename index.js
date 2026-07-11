a80967b1ce1b04263471ffe783e4820c2ca74aa7
Pierreadjemian <Pierreadjemian@gmail.com>
2026-07-11 22:05:52 +0300

    Added NOde file

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const {EventEmitter } = require('events');
+const eventEmitter = new EventEmitter();
+eventEmitter.on('lunch', () =>{
+    console.log('yum')
+})
+eventEmitter.emit('lunch');
+eventEmitter.emit('lunch');
+
+const { readFile , readFileSync } = require('fs');
+const txt = readFileSync ('./hello.txt' , 'utf8' , (err, txt) =>{
+console.log(txt)
+});
+console.log ('do this ASAP')
